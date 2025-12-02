d31401362dd0efe388eae0db86d43c02e981ef14
ale <dev@LAPTOP-GIN2001.localdomain>
2025-12-02 22:05:36 +0700

    hosting vercel

--- surat-app-server/server.js
+++ surat-app-server/server.js
@@ -6,8 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Gunakan rute surat
 app.use('/api/surat', require('./routes/surat'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server generator berjalan di port ${PORT}`));
+
+if (require.main === module) {
+    app.listen(PORT, () => console.log(`Server generator berjalan di port ${PORT}`));
+}
+module.exports = app; 
